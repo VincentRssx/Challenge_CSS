7932f6fba5b39d59b274cc356ac87c20e6ecf34c
VincentRssx <Vincent.rssx59@gmail.com>
2023-10-15 17:02:06 +0200

    challenge Card CSS

---
+++ src/components/Body.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+function Body (){
+const museumArray = [
+    {name : "Museum 1", imgSrc : "https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+{name : "Museum 2", imgSrc : "https://images.pexels.com/photos/460736/pexels-photo-460736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+{name : "Museum 3", imgSrc :"https://images.pexels.com/photos/1839919/pexels-photo-1839919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+]
+
+const [Musée,setMusée] = useState(0);
+
+const HandleClick = (museumTableau) => {
+setMusée(museumTableau)
+}
+
+    return(
+<body>
+    <section>
+    {museumArray.map((museumTableau) =>{
+        return(
+
+            <button onClick={() => HandleClick(museumTableau)}>{museumTableau.name}</button>
+        )})}
+        <h2> CLICK ON THE DIFFERENT BUTTONS TO SEE OUR PARTNER MUSEUMS </h2>
+        <ul>
+        <li><p>{Musée.name}</p>
+        <img className="imageMusé" src={Musée.imgSrc}/> </li>
+    </ul>
+    
+    </section>
+</body>
+    )
+}
+
+export default Body
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header () {
+    return(
+<header>
+<h1> Visit card for Museum</h1>
+</header>
+    )
+}
+
+export default Header
